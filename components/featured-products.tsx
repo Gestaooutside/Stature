98647c1e796bc4da43e6712f7112d020699dc14a
guilhermehbferreira-stack <guilhermehbferreira@gmail.com>
2026-01-10 14:39:28 -0300

    Update featured-products.tsx

--- components/featured-products.tsx
+++ components/featured-products.tsx
@@ -143,7 +143,7 @@ export function FeaturedProducts({ onOpenPaymentModal }: FeaturedProductsProps) 
         </Reveal>
 
         <motion.div
-          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 md:gap-8 lg:gap-10 xl:gap-12"
+          className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 md:gap-6 lg:gap-6"
           initial="hidden"
           animate="visible"
           variants={{
